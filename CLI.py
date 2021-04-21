465fcc53f993306ae07e550b6c65a115376bb5be
Goldstriker208 <47230190+Goldstriker208@users.noreply.github.com>
2021-04-20 18:37:24 -0700

    Full support for GUI

---
+++ CLI.py
@@ -0,0 +1,57 @@
+from dl_paper_jar import dl_jar
+import os
+import time
+import subprocess
+import sys
+
+
+# Ask User Minecraft Server name, version, and min/max RAM
+server_name = str(input("Minecraft Server Name: "))
+mc_version = str(input("Minecraft Version: "))
+dest_folder = server_name
+min_ram = str(input("Min Ram: "))
+max_ram = str(input("Max Ram: "))
+
+# Variables and calls dl_jar function
+json_url = 'https://papermc.io/api/v2/projects/paper/versions/' + mc_version
+filename = json_url.split('/')[-1].replace(" ", "_")  # be careful with file names
+file_path = os.path.join(dest_folder, filename)
+dl_jar(json_url, dest_folder, mc_version)
+
+
+# Creates batch file
+start_java = "java -Xms" + min_ram + "G -Xmx" + max_ram + "G -jar paper.jar nogui"
+myBat = open(server_name + "/" + "start.bat",'w+')
+myBat.write(start_java)
+myBat.close()
+
+
+
+
+# Saves Minecraft Server Info
+file2write=open(dest_folder + "/" + "server-info.txt",'w')
+file2write.write("server-name = " + server_name)
+file2write.write("\nmc-version = " + mc_version)
+file2write.write("\nmin-ram = " + min_ram)
+file2write.write("\nmax-ram = " + max_ram)
+file2write.close()
+
+# Ask user to agree to EULA
+eula = str(input("Type agree if your agree to Mojangs EULA: "))
+if eula == "agree":
+    file2write=open(dest_folder + "/" + "eula.txt",'w')
+    file2write.write("eula=true")
+    file2write.close()
+    
+print("Open the batch file to start your Minecraft Server")
+print(os.path.abspath(server_name))
+time.sleep(1)
+os.system("cd " + (os.path.abspath(server_name)))
+subprocess.Popen("explorer /select," + (os.path.abspath(server_name)))
+
+# startserver = str(input("Would you like to start the Minecraft Server [Y/n] : "))
+
+# if startserver == 'y':
+#     pass
+
+sys.exit()
---
+++ GUI.py
@@ -0,0 +1,158 @@
+import tkinter as tk
+import tkinter.messagebox as msgbox
+import sys 
+
+# global file2write
+
+global server_name
+global mc_version
+
+mc_version = []
+server_name = []
+# server_name = ""
+
+class main_menu():
+    def __init__(self, master):
+        self.master = master
+        # self.btn = tk.Button(master, text="Button", command=self.command)
+        # self.btn.pack()
+        self.label_text = tk.StringVar()
+        self.label_text.set("Choose One")
+        self.label = tk.Label(master, textvar=self.label_text)
+        self.label.pack(fill=tk.BOTH, expand=1, padx=100, pady=30)
+
+        self.paper_button = tk.Button(master, text="Paper", command=self.open_paper_menu)
+        self.paper_button.pack(side=tk.LEFT, padx=(20, 0), pady=(0, 20))
+
+        self.vanilla_button = tk.Button(master, text="Vanilla", command=self.open_vanilla_menu)
+        self.vanilla_button.pack(side=tk.RIGHT, padx=(0, 20), pady=(0, 20))
+
+
+    def open_paper_menu(self):
+        self.master.withdraw()
+        toplevel = tk.Toplevel(self.master)
+        # toplevel.geometry("350x350")
+        app = paper_menu(toplevel)
+
+    def open_vanilla_menu(self):
+        self.master.withdraw()
+        toplevel = tk.Toplevel(self.master)
+        toplevel.geometry("350x350")
+        app = vanilla_menu(toplevel)
+
+class paper_menu:
+    def __init__(self, master):
+        self.master = master
+        # self.frame = tk.Frame(self.master)
+        # self.quitButton = tk.Button(self.frame, text = '1Quit', width = 25, command = self.command)
+        # self.quitButton.pack()
+        # self.frame.pack()
+
+        self.label_text = tk.StringVar()
+        self.label_text.set("Minecraft Server Name: ")
+
+        self.name_text = tk.StringVar()
+
+        
+        self.label = tk.Label(master, textvar=self.label_text)
+        self.label.pack(fill=tk.BOTH, expand=1, padx=100, pady=10)
+
+        self.label2 = tk.Label(master, textvar=self.label_text)
+        self.label.pack(fill=tk.BOTH, expand=1, padx=100, pady=0)
+
+        self.name_entry = tk.Entry(master, textvar=self.name_text)
+        self.name_entry.pack(fill=tk.BOTH, expand=1, padx=20, pady=20)
+
+        next_button = tk.Button(master, text="Next", command=self.server_name)
+        next_button.pack(side=tk.RIGHT, padx=(0, 20), pady=(0, 20))
+
+
+
+    def server_name(self):
+        self.master.withdraw()
+        toplevel = tk.Toplevel(self.master)
+        app = paper_menu2(toplevel)
+        name = self.name_entry.get()
+        file2write=open("server-info.txt",'w')
+        file2write.write("server-name = " + name)
+        file2write.close()
+        server_name.append(name)
+        print("Minecraft Server Name: " + name)
+
+
+
+class paper_menu2:
+    def __init__(self, master):
+        self.master = master
+        self.label_text = tk.StringVar()
+        self.label_text.set("Minecraft Version: ")
+
+        self.name_text = tk.StringVar()
+
+        
+        self.label = tk.Label(master, textvar=self.label_text)
+        self.label.pack(fill=tk.BOTH, expand=1, padx=100, pady=10)
+
+        self.label2 = tk.Label(master, textvar=self.label_text)
+        self.label.pack(fill=tk.BOTH, expand=1, padx=100, pady=0)
+
+        self.name_entry = tk.Entry(master, textvar=self.name_text)
+        self.name_entry.pack(fill=tk.BOTH, expand=1, padx=20, pady=20)
+
+        next_button = tk.Button(master, text="Next", command=self.mc_version)
+        next_button.pack(side=tk.RIGHT, padx=(0, 20), pady=(0, 20))
+
+
+    def mc_version(self):
+        self.master.withdraw()
+        name = self.name_entry.get()
+        file2write=open("server-info.txt",'w')
+        file2write.write("\nmc-version = " + name)
+        file2write.close()
+        paper_menu2.eula(self)
+        name = self.name_entry.get()
+        mc_version.append(name)
+        print("Minecraft Version: " + name)
+
+       
+
+
+    def eula(self):
+        if msgbox.askyesno("Mojangs Eula", "Agree to Mojangs EULA?"):
+            # Yes
+            print("Agreed to EULA")
+            root.quit()
+        else:
+            # No
+            sys.exit()
+            root.quit()
+
+
+
+
+
+
+class vanilla_menu:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.quitButton = tk.Button(self.frame, text = '2Quit', width = 25, command = self.command)
+        self.quitButton.pack()
+        self.frame.pack()
+    def command(self):
+        self.master.withdraw()
+        toplevel = tk.Toplevel(self.master)
+        toplevel.geometry("350x350")
+        app = vanilla_menu(toplevel)
+
+
+
+
+root = tk.Tk()
+root.title("window")
+# root.geometry("350x350")
+cls = main_menu(root)
+root.mainloop()
+
+# print(server_name)
+# print(mc_version)
