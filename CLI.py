a530c0105a9af672c5a3b4680136ffbca18d5859
Will Stein <steinwillb@gmail.com>
2025-07-07 21:39:08 -0700

    removed files

--- CLI.py
+++ CLI.py
@@ -3,156 +3,116 @@ import os
 import time
 import subprocess
 import sys
+import requests
+import json
+import os, sys, stat
+from sys import platform
+import time
 
-interface = "GUI"
+def check_eula(eula):
+    if eula != "agree":
+        print("Did not agree to EULA")
+        print("Exiting..")
+        return False
 
-# Ask User Minecraft Server name, version, and min/max RAM
-mc_server_name = str(input("Minecraft Server Name: "))
-mc_version = str(input("Minecraft Version: "))
-dest_folder = mc_server_name
-min_ram = str(input("Min Ram: "))
-max_ram = str(input("Max Ram: "))
-
-ram = max_ram
-
-# Ask user to agree to EULA
-eula = str(input("Type agree if your agree to Mojangs EULA: "))
-if eula == "agree":
-    eula = True
-else:
-    eula = False
-if eula == False:
-    print("Did not agree to EULA")
-    print("Exiting..")
-    time.sleep(2)
-    sys.exit() 
-else:
-    import requests
-    import json
-    import os, sys, stat
-    import shutil
-    import subprocess
-    from sys import platform
-    import time
-
-    global paper_builds_filename
-    global paper_builds_file_path
-    global mc_paper_versions_filename
-    global mc_paper_versions_file_path
+    return True
 
 
-    # Download Paper Jar file
-    def dl_jar(paper_builds_url: str, mc_paper_versions_url: str, dest_folder: str, mc_version: str):
+# Fetch MC Paper Versions API JSON File
+def fetch_verisons_json():   
 
-        # Create Minecraft Server folder
-        if not os.path.exists(dest_folder):
-            os.makedirs(dest_folder)  # create folder if it does not exist
-            print("Created Folder: " + dest_folder)
+    mc_paper_versions_url = 'https://api.papermc.io/v2/projects/paper/'
 
+    # Filepath
+    print("mc versions: " + mc_paper_versions_file_path)
 
-        # Download Paper Builds JSON
-        r = requests.get(paper_builds_url, allow_redirects=True)
-        print("Downloaded File: JSON " + paper_builds_filename)
+    # Download Paper MC Versions JSON
+    r = requests.get(mc_paper_versions_url, allow_redirects=True)
+    print("Downloaded File: JSON " + mc_paper_versions_filename)
 
-        # Save Paper Builds JSON to file path
-        open(paper_builds_file_path, 'wb').write(r.content)
-        print("Saved JSON " + paper_builds_filename + " to ", os.path.abspath(paper_builds_file_path))
-        with open(paper_builds_file_path, 'r') as file:
-            paper_builds_data = file.read().replace('\n', '')
+    # Save Paper MC Versions JSON to file path
+    open(mc_paper_versions_file_path, 'wb').write(r.content)
+    print("Saved JSON " + mc_paper_versions_filename + " to ", os.path.abspath(mc_paper_versions_file_path))
+    with open(mc_paper_versions_file_path, 'r') as file:
+        mc_paper_versions_data = file.read().replace('\n', '')
 
+    readjson_paper_versions = json.loads(mc_paper_versions_data)["versions"]
 
-        # Filepaths
-        print("mc versions: " + mc_paper_versions_file_path)
-        print("paper builds: " + paper_builds_file_path)
+    return readjson_paper_versions
+    
 
-        # Download Paper MC Versions JSON
-        r = requests.get(mc_paper_versions_url, allow_redirects=True)
-        print("Downloaded File: JSON " + mc_paper_versions_filename)
+def fetch_builds_json():
 
-        # Save Paper MC Versions JSON to file path
-        open(mc_paper_versions_file_path, 'wb').write(r.content)
-        print("Saved JSON " + mc_paper_versions_filename + " to ", os.path.abspath(mc_paper_versions_file_path))
-        with open(mc_paper_versions_file_path, 'r') as file:
-            mc_paper_versions_data = file.read().replace('\n', '')
+    paper_builds_url = f'https://api.papermc.io/v2/projects/paper/versions/{mc_version}'
 
+    # Filepath
+    print("paper builds: " + paper_builds_file_path)
 
-        # Read JSON Files
-        class readjson:
-            def __init__(self, json_def):
-                self.__dict__ = json.loads(json_def)
+    # Download Paper Builds JSON
+    r = requests.get(paper_builds_url, allow_redirects=True)
+    print("Downloaded File: JSON " + paper_builds_filename)
 
-        readjson_paper_builds = readjson(paper_builds_data)
-        readjson_paper_versions = readjson(mc_paper_versions_data)
-
-        global mc_paper_versions
-        mc_paper_versions = readjson_paper_versions.versions + readjson_paper_versions.version_groups
-
-        
-        def check_vaild_mc_version():
-            vaild_mc_version = None
-            for mc_paper_version in mc_paper_versions:
-                if mc_version == mc_paper_version:
-                    vaild_mc_version = True
-                    print("Vaild MC Paper Version")
-                    return True
-
-            if mc_version != mc_paper_version: 
-                return False
-                sys.exit()
-                
-
-        if check_vaild_mc_version() == True:
-
-            # Download Latest Paper Build
-            latest_build = readjson_paper_builds.builds[-1]
-            print("Downloading Latest Build: " + str(latest_build))
-            jar_file = "paper-" + mc_version + "-" + str(latest_build) + ".jar"
-            dl_paper_jar = "https://papermc.io/api/v2/projects/paper/versions/" + str(mc_version) + "/builds/" + str(latest_build) + "/downloads/" + jar_file
-            r = requests.get(dl_paper_jar, allow_redirects=True)
-            print("Downloaded: " + jar_file)
-            open(dest_folder + "/" + jar_file, 'wb').write(r.content)
-            print("Saved " + jar_file + " to", os.path.abspath(paper_builds_file_path))
-            os.rename(dest_folder + "/" + jar_file, dest_folder + "/" + "paper.jar")
-
-        else:
-            print("Invaild MC Version")
+    # Save Paper Builds JSON to file path
+    open(paper_builds_file_path, 'wb').write(r.content)
+    print("Saved JSON " + paper_builds_filename + " to ", os.path.abspath(paper_builds_file_path))
+    with open(paper_builds_file_path, 'r') as file:
+        paper_builds_data = file.read().replace('\n', '')
 
 
+    readjson_paper_builds = json.loads(paper_builds_data)["builds"]
+
+    return readjson_paper_builds
+
+def check_vaild_mc_version():
+
+    for mc_paper_version in fetch_verisons_json():
+        if mc_version == mc_paper_version:
+            print("Vaild MC Paper Version")
+            return True
+
+    if mc_version != mc_paper_version: 
+        return False
 
 
-    dest_folder = mc_server_name
+# Create MC Server Directory
+def mc_server_dir(): 
 
-    # Set defualt ram
-    min_ram = ram
-    max_ram = ram
+    # Create Minecraft Server folder
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)  # create folder if it does not exist
+        print("Created Folder: " + dest_folder)
 
-    # Variables and calls dl_jar function
+def download_jar():
 
-    # JSON URLS
-    paper_builds_url = 'https://papermc.io/api/v2/projects/paper/versions/' + mc_version
-    mc_paper_versions_url = 'https://papermc.io/api/v2/projects/paper/'
+    if check_vaild_mc_version() == True:
 
-    # File names
-    paper_builds_filename = "Paper Builds"
-    mc_paper_versions_filename = "MC Paper Versions"
+        # Download Latest Paper Build
+        latest_build = fetch_builds_json()[-1]
+        print("Downloading Latest Build: " + str(latest_build))
+        jar_file = "paper-" + mc_version + "-" + str(latest_build) + ".jar"
+        dl_paper_jar = "https://api.papermc.io/v2/projects/paper/versions/" + str(mc_version) + "/builds/" + str(latest_build) + "/downloads/" + jar_file
+        r = requests.get(dl_paper_jar, allow_redirects=True)
+        print("Downloaded: " + jar_file)
+        open(dest_folder + "/" + jar_file, 'wb').write(r.content)
+        print("Saved " + jar_file + " to", os.path.abspath(paper_builds_file_path))
+        os.rename(dest_folder + "/" + jar_file, dest_folder + "/" + "paper.jar")
 
-    #File paths
-    paper_builds_file_path = os.path.join(dest_folder, paper_builds_filename)
-    mc_paper_versions_file_path = os.path.join(dest_folder, mc_paper_versions_filename)
+    else:
+        print("Invaild MC Version")
 
-    dl_jar(paper_builds_url, mc_paper_versions_url, dest_folder, mc_version)
+def start_script():
 
     # Start Java
-    start_java = "java -Xms" + min_ram + "G -Xmx" + max_ram + "G -jar paper.jar nogui"
+    # start_java = "java -Xms" + min_ram + "G -Xmx" + max_ram + "G -jar paper.jar nogui"
+    start_java = f"java -Xms {min_ram}G -Xmx {max_ram}G -jar paper.jar nogui"
 
     # Creates batch script file
-
-    myBat = open(mc_server_name + "/start.bat",'w+')
+    myBat = open(f"{mc_server_name}/start.bat",'w+')
     myBat.write(start_java)
     myBat.close()
-    
+
     # Creates shell script file
-    myBat = open(mc_server_name + "/" + "start.sh",'w+')
+    myBat = open(f"{mc_server_name}/start.sh",'w+')
     myBat.write("#!/bin/sh\n")
     myBat.write('cd "$(dirname "$0")"\n')
     myBat.write("")
@@ -160,29 +120,27 @@ else:
     myBat.close()
     #os.chmod(mc_server_name + "/start.sh", stat.S_IRWXO )
 
-    st = os.stat(mc_server_name + "/start.sh")
-    os.chmod(mc_server_name + "/start.sh", st.st_mode | stat.S_IEXEC)
+    st = os.stat(f"{mc_server_name}/start.sh")
+    os.chmod(f"{mc_server_name}/start.sh", st.st_mode | stat.S_IEXEC)
 
     # Saves Minecraft Server Info
-    file2write=open(dest_folder + "/" + "server-info.txt",'w')
+    file2write=open(f"{mc_server_name}/server-info.txt",'w')
     file2write.write("server-name = " + mc_server_name)
     file2write.write("\nmc-version = " + mc_version)
-    file2write.write("\nmin-ram = " + min_ram)
-    file2write.write("\nmax-ram = " + max_ram)
+    file2write.write(f"\nmin-ram = {min_ram}")
+    file2write.write(f"\nmax-ram = {max_ram}")
     file2write.close()
 
-    if eula == True:
-        file2write=open(dest_folder + "/" + "eula.txt",'w')
-        file2write.write("eula=true")
-        file2write.close()
+    # Set Eula
+    file2write=open(dest_folder + "/" + "eula.txt",'w')
+    file2write.write("eula=true")
+    file2write.close()
 
     print("Open the batch script or shell script file to start your Minecraft Server")
     print(os.path.abspath(mc_server_name))
 
     time.sleep(1)
     os.system("cd " + (os.path.abspath(mc_server_name)))
-
-
 
     if platform == "linux":
         pass
@@ -192,6 +150,47 @@ else:
         subprocess.Popen("explorer /select," + (os.path.abspath(mc_server_name)))
 
 
+# Ask user to agree to EULA
+eula_input = str(input("Type agree if your agree to Mojangs EULA: ")).strip().lower()
 
-    # Quit
+if not check_eula(eula_input):
+    time.sleep(2)
     sys.exit()
+
+# mc_server_name = str(input("Minecraft Server Name: "))
+mc_server_name = "mc-server"
+
+# mc_version = str(input("Minecraft Version: "))
+mc_version = "1.12.2"
+
+# dest_folder = mc_server_name
+dest_folder = "mc-server"
+
+print("Type only the numbers of RAM(no GB at end)")
+min_ram = str(input("Min Ram: "))
+max_ram = str(input("Max Ram: "))
+
+# File names
+paper_builds_filename = "Paper Builds"
+mc_paper_versions_filename = "MC Paper Versions"
+
+#File paths
+paper_builds_file_path = os.path.join(dest_folder, paper_builds_filename)
+mc_paper_versions_file_path = os.path.join(dest_folder, mc_paper_versions_filename)
+
+# Fetch JSON Files
+fetch_verisons_json()
+fetch_builds_json()
+
+# Create MC Directory
+mc_server_dir()
+
+# Download paper jar file
+download_jar()
+
+# Create Start Script
+start_script()
+
+
+# Quit
+sys.exit()
